a347ede62dfefd0ec2af6b10e902ed109b545d99
Zhangmei Li <lizhangmei@baidu.com>
2018-08-19 10:40:29 +0800

    speed up tinkerpop test
    how to:
    1.speed up rocksdb backend by truncating tables
    2.don't need to clear if the database is empty
    3.don't need to clear variables if it's empty
    4.don't need to commit hbase each len-prefix for delete index-label
    
    potential problems:
    1.mysql may block if create task vertex-label when truncating table
    2.rocksdb may miss CF if create task vertex-label when truncating table
    3.hbase may block if truncate hbase with version<2.0
    
    improve #14
    
    Change-Id: I4b2393aea8b0fc63c1886e984a576a1f5808b25c

--- hugegraph-test/src/main/java/com/baidu/hugegraph/tinkerpop/TestGraph.java
+++ hugegraph-test/src/main/java/com/baidu/hugegraph/tinkerpop/TestGraph.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.configuration.Configuration;
 import org.apache.tinkerpop.gremlin.process.computer.GraphComputer;
@@ -35,9 +36,11 @@ import org.apache.tinkerpop.gremlin.structure.io.Io;
 
 import com.baidu.hugegraph.HugeGraph;
 import com.baidu.hugegraph.io.HugeGraphIoRegistry;
+import com.baidu.hugegraph.schema.PropertyKey;
 import com.baidu.hugegraph.schema.SchemaManager;
 import com.baidu.hugegraph.structure.HugeFeatures;
 import com.baidu.hugegraph.type.define.IdStrategy;
+import com.google.common.collect.ImmutableSet;
 
 @Graph.OptIn("com.baidu.hugegraph.tinkerpop.StructureBasicSuite")
 @Graph.OptIn("com.baidu.hugegraph.tinkerpop.ProcessBasicSuite")
@@ -46,6 +49,9 @@ import com.baidu.hugegraph.type.define.IdStrategy;
 public class TestGraph implements Graph {
 
     public static final String DEFAULT_VL = "vertex";
+
+    public static final Set<String> TRUNCATE_BACKENDS =
+           ImmutableSet.of("rocksdb", "mysql", "scylladb");
 
     private static volatile int id = 666;
 
@@ -74,6 +80,32 @@ public class TestGraph implements Graph {
         this.initedBackend = false;
     }
 
+    protected void clearAll(String testClass) {
+        List<PropertyKey> pks = this.graph.schema().getPropertyKeys();
+        if (pks.isEmpty()) {
+            // No need to clear if there is no PKs(that's no schema and data)
+            return;
+        }
+
+        if (TRUNCATE_BACKENDS.contains(this.graph.backend())) {
+            // Delete all data by truncating tables
+            this.truncateBackend();
+        } else {
+            // Clear schema (also include data)
+            this.clearSchema();
+
+            // Clear variables if needed (would not clear when clearing schema)
+            if (testClass.endsWith("VariableAsMapTest")) {
+                this.clearVariables();
+                this.tx().commit();
+            }
+        }
+    }
+
+    protected void truncateBackend() {
+        this.graph.truncateBackend();
+    }
+
     protected void clearSchema() {
         // Clear schema and graph data will be cleared at same time
         SchemaManager schema = this.graph.schema();
@@ -98,6 +130,10 @@ public class TestGraph implements Graph {
     protected void clearVariables() {
         Variables variables = this.variables();
         variables.keys().forEach(key -> variables.remove(key));
+    }
+
+    protected boolean closed() {
+        return this.graph.closed();
     }
 
     @Override
